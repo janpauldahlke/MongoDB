e2992b1fa3111ca4b9dfe74ba748a05c55a0a77f
janpauldahlke <janpauldahlke@gmail.com>
2017-05-06 12:57:51 +0200

    make mocha accesible for npm run test

--- first_attempt/src/user.js
+++ first_attempt/src/user.js
@@ -5,3 +5,17 @@ const mongoose = require('mongoose');
 
 //pull specific property of the mongoose object -> schema
 const Schema =mongoose.Schema;
+
+//define user and use schema object with config
+const UserSchema = new Schema({
+  //key : datatype
+  name: String
+});
+
+//init user model, name it, pass UserSchema
+//terminolgy userModel = userClass // is not an instance!! represents entire collection of data
+//mongoose magic *puffPuff
+const User = mongoose.model('user', UserSchema);
+
+//AMD pattern export userClass to global scope
+module.exports = User;
---
+++ first_attempt/test/create_test.js
@@ -0,0 +1,14 @@
+/*TODO
+ test creation of new user and save ot to the database
+*/
+//args: 1.string with test desc, actual testsuites with it BLOCKS
+
+//comes with mocha
+const assert = require('assert');
+
+describe('Creating records', () => {
+  //args: string descript, func with the assertion of test
+  it('saves a user', () => {
+    assert(1+1===2);
+  });
+});
