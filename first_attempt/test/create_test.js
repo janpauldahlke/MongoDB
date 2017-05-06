f612a0a02205735c69e5507c26fbe19fb8ad61b8
janpauldahlke <janpauldahlke@gmail.com>
2017-05-06 13:12:00 +0200

    instance and save first user

--- first_attempt/test/create_test.js
+++ first_attempt/test/create_test.js
@@ -1,14 +1,24 @@
 /*TODO
- test creation of new user and save ot to the database
+ 1. test creation of new user
+ 2. save it to the database
+ 3. test existence of createt user istance
 */
 //args: 1.string with test desc, actual testsuites with it BLOCKS
 
 //comes with mocha
 const assert = require('assert');
+//import user
+//make it capital User, to show its a class while instance will be user lowercased
+const User = require('../src/user');
 
 describe('Creating records', () => {
   //args: string descript, func with the assertion of test
   it('saves a user', () => {
-    assert(1+1===2);
+      //step 1 of TODO
+      //instance object of User!! oneliner, spread it with more attribs later on
+      //but jan will not be saved /persisted
+      const jan = new User({ name: 'Jan' });
+      //step 2 of TODO
+      jan.save();
   });
 });
