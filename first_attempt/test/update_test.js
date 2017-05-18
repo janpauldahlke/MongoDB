d70979281bfd683a490ad59351e53b50104052d4
janpauldahlke <janpauldahlke@gmail.com>
2017-05-18 11:19:01 +0200

    added helper AssertName() function

--- first_attempt/test/update_test.js
+++ first_attempt/test/update_test.js
@@ -3,7 +3,7 @@ const User = require('../src/user');
 
 describe('update records in the Database', () => {
   let paul;
-
+  const newName = 'Paul Johannes'
   //create some entry to test against
   beforeEach((done) => {
       paul = new User({name: 'Paul'});
@@ -14,9 +14,20 @@ describe('update records in the Database', () => {
   });
 
 
+  //a helper function to assert to prevent code duplicates
+  //this could also be rewritten for read_test.js etc
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === newName);
+        done();
+      });
+  }
+
   //set and save
   it('instance type set and save', (done) => {
-      let newName = 'Paul Johannes'
       //console.log(paul);
       paul.set('name',newName);
       /*paul.save()
@@ -25,15 +36,18 @@ describe('update records in the Database', () => {
           console.log(paul.name);
           done();
         });*/
-      paul.save()
+
+        //refactored into helper Function to avoid code duplicates
+      /*paul.save()
         //get all users and assert that the name is = newName
-        .then(() => User.find({/*empty object returns all Users*/}))
+        .then(() => User.find({/*empty object returns all Users*///}))
         //arg user contains callback from previous then
-        .then((users) => {
+        /*.then((users) => {
           assert(users.length === 1);
           assert(users[0].name === newName);
           done();
-        });
+        });*/
+        assertName(paul.save(), done);
       });
 /*
 a practical approach how to use set and save
@@ -46,4 +60,8 @@ this would work on dynamicly and the logic to change would be in the functions
 and save would be called in the end, w/o touching our database twice
 */
 
+  //model instance can update
+it('a model instance can update', (done) => {
+    assertName(paul.update({name: newName}), done);
+  });
 });
