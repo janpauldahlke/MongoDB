35afaf9c1c97447b421878afc3515ba332247dc5
janpauldahlke <janpauldahlke@gmail.com>
2017-05-30 13:39:39 +0200

    add post to existing record

--- first_attempt/test/subdocument_test.js
+++ first_attempt/test/subdocument_test.js
@@ -18,4 +18,34 @@ describe('subdocuments', () => {
           done();
         });
     });
+
+    it('adding a post to existng record', (done) => {
+        //TODO create paul, save paul, ftech paul, add post to paul, save paul, ftech paul, assert post
+
+        //create paul
+        const paul = new User({
+          name: 'Paul',
+          //initiate empty array of post
+          posts: []
+        });
+        //save paul
+        paul.save()
+
+        //ftech paul
+          .then(() => User.findOne({name: 'Paul'}))
+          .then((user) => {
+            //add post to paul -> Array.push
+            user.posts.push({ title: 'a newly pushed post onto existing user' });
+            //save modififed paul
+            //care! here is return for to make the next promise chainable, otherwise it would not work, cuz the function sctope doesnt give back a callback
+            return user.save();
+          })
+          //ftech again
+          .then(() => User.findOne({ name: 'Paul' }))
+          //assert part
+          .then((user) => {
+              assert(user.posts[0].title === 'a newly pushed post onto existing user');
+              done();
+          });
+      });
 });
