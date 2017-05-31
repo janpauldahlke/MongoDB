dca382cb62109520aded3db98e5a415b0231acff
janpauldahlke <janpauldahlke@gmail.com>
2017-05-31 15:14:28 +0200

    prepare testcase by writing beforeEach

--- first_attempt/test/association_test.js
+++ first_attempt/test/association_test.js
@@ -4,8 +4,32 @@ const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Associations', () => {
+  //same vars cuz iam lazy
+  let paul, blogPost, comment;
+  const contentQuestion = 'http://www.theasciicode.com.ar/ascii-printable-characters/asterisk-ascii-code-42.html';
+
+  beforeEach((done) => {
+    paul = new User({name: 'Paul'});
+    blogPost = new BlogPost({title: 'the meaning of life', content: contentQuestion});
+    comment = new Comment({content: 'this might be valid.'});
+
+    //TODO associate paul with blogPost
+    //because blogPosts is an array on joe, so just push it // some mongoose magic!?
+
+    joe.blogPosts.push(blogPost);
+
+    //TODO associate blogPost with comment
+    blogPost.comments.push(comment);
+
+  });
+
+  /*it('', () =>{
+
+  });
+
 
   it('', () =>{
 
   });
+  */
 });
