6d3494e1e11127343e6e0ea7252aa65f872e956d
janpauldahlke <janpauldahlke@gmail.com>
2017-06-13 17:42:30 +0200

    unretire

--- allMusics/database/queries/SetNotRetired.js
+++ allMusics/database/queries/SetNotRetired.js
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves after the update
  */
 module.exports = (_ids) => {
+  /*return Artist.updateMany(
+    { _id: {$in : _ids} },
+    {retired : false}
+  );*/
+
+  //difference between updateMany() and update(y,x, {multi : true})
+
+  return Artist.update(
+    { _id: {$in : _ids} },
+    {retired : false},
+    { multi: true}
+  );
 };
