8ff649bd1bb8ca07afa07babee0dd4add6591695
janpauldahlke <janpauldahlke@gmail.com>
2017-06-15 08:03:32 +0200

    mocha mongoose done() problem

--- exposeAPI/models/driver.js
+++ exposeAPI/models/driver.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DriverSchema = new Schema({
-  /*name: {
+  name: {
     type: String,
     required : true
-  },*/
+  },
   email : {
     type: String,
     required : true
--- exposeAPI/test/controllers/drivers_controller_test.js
+++ exposeAPI/test/controllers/drivers_controller_test.js
@@ -1,19 +1,32 @@
 const assert = require('assert');
 const request = require('supertest');
 const DriverController = require('../../controllers/drivers_controller');
+const mongoose = require('mongoose');
 const app = require('../../app');
+//workaround for mocha
+const Driver = mongoose.model('driver');
 
+//fight deprecation
+mongoose.Promise = global.Promise;
 
 describe('Drivers Controller', () => {
+
   it('POST to /api/drivers creates a new driver in DB', (done) => {
-    request(app)
-      .post('/api/drivers')
-      //send data on post no send but more an add to
-      .send({email : 'paul@paulspaul.paul'})
-      .end(() => {
-        //console.log(response.body);
-        //assert(response.body.name === 'Paul');
-        done();
-      });
+    //count driver before and after insert
+    Driver.count().then(count => {
+      request(app)
+        .post('/api/drivers')
+        //send data on post no send but more an add to
+        .send({name : 'Paul', email : 'paul@paulspaul.paul'})
+        .end(() => {
+          Driver.count().then(newCount => {
+            assert(count+1 === newCount);
+            //done() is not excercises
+            //maybe https://www.npmjs.com/package/mocha-mongoose ??
+            done();
+          });
+        });
+    });
   });
+
 });
